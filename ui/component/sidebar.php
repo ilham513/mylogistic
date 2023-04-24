2ad5da6cef685dc56584ceb76fb39393950b79e8
Ilham513 <ilham513@outlook.com>
2023-04-24 11:34:15 +0700

    first commit

---
+++ ui/component/sidebar.php
@@ -0,0 +1,21 @@
+<!-- Sidebar -->
+    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+      <div class="position-sticky">
+        <div class="list-group list-group-flush mx-3 mt-4">
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple active" aria-current="true">
+            <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Dashboard</span>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple">
+            <i class="fas fa-motorcycle fa-fw me-3"></i><span>Kurir </span>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
+              class="fas fa-box fa-fw me-3"></i><span>Gudang</span></a>
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
+              class="fas fa-user fa-fw me-3"></i><span>Pelanggan</span></a>
+          <a href="#" class="list-group-item list-group-item-action py-2 ripple">
+            <i class="fas fa-truck fa-fw me-3"></i><span>Pengiriman</span>
+          </a>
+        </div>
+      </div>
+    </nav>
+    <!-- Sidebar -->
