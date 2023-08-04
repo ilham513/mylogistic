80356f42610437752f5bf7ca694b3dd579cd62c7
Ilham513 <ilham513@outlook.com>
2023-08-04 20:10:06 +0700

    first commit

--- mylogistic.sql
+++ mylogistic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2023 at 03:04 PM
+-- Generation Time: Aug 04, 2023 at 03:09 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
